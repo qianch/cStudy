d371d60a76a36a07e08c932eea14f554904534bd
mark.qian <qianchenchn@foxmail.com>
2023-02-13 14:00:23 +0800

    printf

--- src/c/main.c
+++ src/c/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello CMake!");
-
+  printf("Hello CMake! \n");
+  int a = 1;
+  int *b = &a;
+  int c = 3;
+  printf("a:%d \n", a);
+  printf("*b:%d \n", *b);
+  printf("c:%d \n", c);
+  printf("++a: %d \n" ,++a);
   return 0;
 }
