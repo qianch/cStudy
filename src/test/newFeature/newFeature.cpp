abffb366a26c762f02e6986b3a0f808214f3af48
qian.chen <qianchenchn@foxmail.com>
2024-05-19 19:04:00 +0800

    std::find

--- src/test/newFeature/newFeature.cpp
+++ src/test/newFeature/newFeature.cpp
@@ -11,19 +11,20 @@ int main()
     auto str = "Hello"; // 推断str为字符串型
 
     std::vector<int> numbers = {1, 2, 3, 4, 5};
-    for (int num : numbers)
-    {
-        std::cout << num << " ";
-    }
 
-    for (auto num : numbers)
-    {
-        std::cout << num << " ";
-    }
-
-    // 使用Lambda表达式打印每个元素
-    std::for_each(numbers.begin(), numbers.end(), [](int num)
-                  { std::cout << num << " "; });
+	int target = 4;
+	auto it = std::find(numbers.begin(), numbers.end(), target);
+	if (it != numbers.end()) {
+		std::cout << "find:" << target << ",index:" << std::distance(numbers.begin(), it) << std::endl;
+	}
+	else {
+		std::cout << "not find" << target << std::endl;
+	}
+	// 使用Lambda表达式打印每个元素
+	std::for_each(numbers.begin(), numbers.end(), [](int num)
+	{
+		std::cout << num << std::endl;
+	});
 
     std::thread t(printMessage);
     std::cout << "Hello from main thread!" << std::endl;
