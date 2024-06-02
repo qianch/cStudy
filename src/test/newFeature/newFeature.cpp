9716a996192a1764c42bc6e22d52769191e42ab9
qian.chen <qianchenchn@foxmail.com>
2024-06-02 18:42:41 +0800

    std关联式容器map

--- src/test/newFeature/newFeature.cpp
+++ src/test/newFeature/newFeature.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <deque>
 #include <list>
+#include <map>
 
 // 线程函数
 void printMessage()
@@ -118,5 +119,22 @@ int main()
 	last = 20;
 	std::cout << lst.front() << " " << lst.back() << std::endl;
 
+	// 创建空 map 容器，默认根据个键值对中键的值，对键值对做降序排序
+	std::map<std::string, std::string, std::greater<std::string>> myMap;
+	// 调用 emplace() 方法，直接向 myMap 容器中指定位置构造新键值对
+	myMap.emplace("C语言教程", "http://c.biancheng.net/c/");
+	myMap.emplace("Python教程", "http://c.biancheng.net/python/");
+	myMap.emplace("STL教程", "http://c.biancheng.net/stl/");
+	// 输出当前 myMap 容器存储键值对的个数
+	std::cout << "myMap size==" << myMap.size() << std::endl;
+	// 判断当前 myMap 容器是否为空
+	if (!myMap.empty())
+	{
+		// 借助 myMap 容器迭代器，将该容器的键值对逐个输出
+		for (auto i = myMap.begin(); i != myMap.end(); ++i)
+		{
+			std::cout << i->first.data() << " " << i->second.data() << std::endl;
+		}
+	}
 	return 0;
 }
